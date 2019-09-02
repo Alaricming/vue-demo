e92ce000837282759b9e44e339b6ebe68a556177
alaric <xin.ming@metlife.com>
2019-09-02 18:31:22 +0800

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>{{name}}</p>
+    <p>{{age}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Test",
+  props: ["age"],
+  data() {
+    return {
+      name: "haha"
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
